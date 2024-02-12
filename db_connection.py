0cd135d7bf6d22596ec48e9e2669f61a13c46956
Varun Hosadurga Harish <127836353+VarunHarish98@users.noreply.github.com>
2024-02-12 16:39:14 -0500

    Updated DB

--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,7 @@
 import pymongo
 
-url = 'mongodb://localhost:27017'
+url = 'mongodb+srv://varunharish98:qByFNzT0dZvqT72V@cluster0.ava1wr9.mongodb.net/'
+# url = 'mongodb://varunharish98:qByFNzT0dZvqT72V@ac-uijhm4w-shard-00-01.ava1wr9.mongodb.net:27017/your_database_name'
 client = pymongo.MongoClient(url)
 
 db = client['dashboard']
